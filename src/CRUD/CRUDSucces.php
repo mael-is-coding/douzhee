981320d96bb722ba7ca5d142de4a9c358e3eeb6c
Nacola04 <92257036+Nacola04@users.noreply.github.com>
2024-11-10 20:00:52 +0100

    Classe Succes + CRUD correspondant

---
+++ src/CRUD/CRUDSucces.php
@@ -0,0 +1,38 @@
+<?PHP 
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/Douzhee/src/Classes/Succes.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/Douzhee/src/Utils/headerConnection.php";
+
+    //FONCTIONS READ
+
+    /**
+     * Récupère tous les succès
+     * @author Nathan
+     * @return array
+     */
+    function readAllSucces(): array{
+        $connection = connection();
+
+        $readSucces = 'SELECT * FROM succes';
+
+        $statement = $connection->prepare($readSucces);
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Récupère un succès en fonction de son id
+     * @author Nathan
+     * @param int $id
+     * @return Succes
+     */
+    function readSuccesById(int $id): Succes{
+        $connection = connection();
+
+        $readSucces = 'SELECT * FROM succes WHERE id = :id';
+        $statement = $connection->prepare($readSucces);
+        $statement->bindParam(':id', $id);
+        $statement->execute();
+
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return new Succes($results['id'], $results['name'], $results['condition'], $results['type']);
+    }
---
+++ src/Classes/Succes.php
@@ -0,0 +1,84 @@
+<?PHP
+    Class Succes{
+        private int $id;
+        private String $name;
+        private String $condition;
+        private String $type;
+    
+        /**
+         * @param int $id
+         * @param string $name
+         * @param string $condition
+         * @param string $type
+         */
+        public function __construct(int $id, string $name, string $condition, string $type) {
+            $this->id = $id;
+            $this->name = $name;
+            $this->condition = $condition;
+            $this->type = $type;
+        }
+
+        /**
+         * @return int
+         */
+        public function getId(): int {
+            return $this->id;
+        }
+        
+        /**
+         * @param int $id 
+         * @return self
+         */
+        public function setId(int $id): self {
+            $this->id = $id;
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getName(): string {
+            return $this->name;
+        }
+        
+        /**
+         * @param string $name 
+         * @return self
+         */
+        public function setName(string $name): self {
+            $this->name = $name;
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getCondition(): string {
+            return $this->condition;
+        }
+        
+        /**
+         * @param string $condition 
+         * @return self
+         */
+        public function setCondition(string $condition): self {
+            $this->condition = $condition;
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getType(): string {
+            return $this->type;
+        }
+        
+        /**
+         * @param string $type 
+         * @return self
+         */
+        public function setType(string $type): self {
+            $this->type = $type;
+            return $this;
+        }
+}

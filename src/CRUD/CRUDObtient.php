f29a842516a4cf06299f423a5a0d9c9467a28f03
Nacola04 <92257036+Nacola04@users.noreply.github.com>
2024-11-14 15:22:25 +0100

    refactor CRUD obtient

--- src/CRUD/CRUDObtient.php
+++ src/CRUD/CRUDObtient.php
@@ -8,8 +8,8 @@
     /**
      * @brief Crée un lien entre un succès et un joueur pour indiquer qu'il a validé le succès
      * @author Nathan
-     * @param int $idUser
-     * @param int $idSucces
+     * @param int $idUser identifiant du joueur
+     * @param int $idSucces identifiant du succès obtenu
      * @return void
      */
     function createObtient(int $idUser, int $idSucces){
@@ -31,7 +31,7 @@
     /**
      * @brief Récupère la liaison entre un joueur et ses succès
      * @author Nathan
-     * @param int $idUser
+     * @param int $idUser identifiant du joueur
      * @return array
      */
     function readAllSuccesOfAnUser(int $idUser): array{
@@ -49,7 +49,7 @@
     /**
      * @brief Récupère la liaison entre un succès et les joueurs qui l'ont obtenu
      * @author Nathan
-     * @param int $idSucces
+     * @param int $idSucces identifiant du succès
      * @return array
      */
     function readAllUserWinTheSuccesId(int $idSucces): array{

5a48362b1a93fe821a0153b3fde7a73c6f93f5dd
Cosmos-12 <maelsalem@outlook.com>
2024-11-20 16:39:01 +0100

    Création et ajout du CRUD et de la classe

---
+++ src/Classes/ParticiperA.php
@@ -0,0 +1,40 @@
+<?
+
+class ParticiperA {
+
+    private int $idJoueurJoue;
+    private int $idPartieJoue;
+    private int $idJoueur;
+
+
+    function __construct(int $idJJ, int $idPJ, int $idJ) {
+        $this->idJoueurJoue = $idJJ;
+        $this->idPartieJoue = $idPJ;
+        $this->idJoueur = $idJ;
+    }
+
+
+    function getIdJoueurJoue(): int {
+        return $this->idJoueurJoue;
+    }
+
+    function setIdJoueurJoue(int $idJJ) : void {
+        $this->idJoueurJoue = $idJJ;
+    }
+
+    function getIdPartieJoue():int {
+        return $this->idPartieJoue;
+    }
+
+    function setIdPartieJoue(int $idPJ) : void {
+        $this->idPartieJoue = $idPJ;
+    }
+
+    function getIdJoueur() : int {
+        return $this->idJoueur;
+    } 
+
+    function setIdJoueur(int $idJ) : void {
+        $this->idJoueur = $idJ;
+    }
+}

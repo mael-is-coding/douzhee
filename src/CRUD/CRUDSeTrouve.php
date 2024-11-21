11c1b097b9e6407bffba815be9824a2f03e45278
Cosmos-12 <maelsalem@outlook.com>
2024-11-21 16:04:22 +0100

    CRUDSeTrouve

---
+++ src/CRUD/CRUDSeTrouve.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @brief Créé une nouvelle occurence de SeTrouve dans la table se trouve avec les paramètres
+ * @param int $idJ id d'un joueur
+ * @param int $idC id du classement
+ * @return bool
+ */
+function createSeTrouve(int $idJ, int $idC) : bool {
+    $connexion = ConnexionSingleton::getInstance();
+
+    $SelectQuery = "SELECT * FROM setrouve WHERE idJoueur = :idJ AND idClassement = :idC";
+
+    $statement = $connexion->prepare($SelectQuery);
+
+    $statement->bindParam("idJ", $idJ);
+    $statement->bindParam("idC", $idC);
+
+    return $statement->execute();
+}
+
+/**
+ * @brief retourne un objet SeTrouve selon les paramètres passés
+ * @param int $idJ id d'un joueur
+ * @param int $idC id du classement
+ * @return SeTrouve|null
+ */
+function readSeTrouve(int $idJ, int $idC): ?SeTrouve {
+    $connexion = ConnexionSingleton::getInstance();
+
+    $InsertQuery = "INSERT INTO setrouve (idJoueur, idClassement) VALUES (idJoueur = :idJ, idClassement = :idC)";
+
+    $statement = $connexion->prepare($InsertQuery);
+
+    $statement->bindParam("idJ", $idJ);
+    $statement->bindParam("idC", $idC);
+
+    if ($statement->execute()) {
+        
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        if (gettype($results) == "boolean") {
+            $idJ_ = $results ["idJoueur"];
+            $idC_ = $results ["idClassement"];
+        
+            return new SeTrouve($idJ_,$idC_);
+        } else {
+            return null;
+        }
+
+    } else { 
+        return null;
+    }
+}
---
+++ src/Classes/SeTrouve.php
@@ -0,0 +1,28 @@
+<?php
+
+class SeTrouve {
+
+    private int $idJoueur;
+    private int $idClassement;
+
+    function __construct(int $idJ, int $idC) {
+        $this->idClassement = $idC;
+        $this->idJoueur = $idJ;
+    }
+
+    function getIdJoueur() : int {
+        return $this->idJoueur;
+    }
+
+    function getIdClassement() : int {
+        return $this->idClassement;
+    }
+
+    function setIdClassement(int $idC): void {
+        $this->idClassement = $idC;
+    }
+    
+    function setIdJoueur(int $idJ) : void {
+        $this->idJoueur = $idJ;
+    } 
+}

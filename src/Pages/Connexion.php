c9b70bf9afefeee28f22b0c538453ef87b8e791f
Milanor38 <65493965+Milanor38@users.noreply.github.com>
2024-11-22 19:54:54 +0100

    Update Connexion.php

--- src/Pages/Connexion.php
+++ src/Pages/Connexion.php
@@ -1,6 +1,8 @@
 <?php
     require_once("../CRUD/CRUDJoueur.php");
     require_once("../Utils/headerInit.php");
+    $cookiename = "Email";
+    $cookiename2 = "Password";
 ?>
 
     <link rel="stylesheet" href="../../assets/css/styleCIRV.css"> 
@@ -9,8 +11,8 @@
     <div class="PCIR">
         <h2>Connexion</h2>
         <form action = "Connexion.php" method="POST">
-            <input name="E-mail" type="email" placeholder="E-mail" required value="<?php echo $_SESSION['cacheE-mail'] ?? '';?>">
-            <input name = "Password" type="password" placeholder="Password" required value="<?php echo $_SESSION['cachePassword'] ?? '';?>">
+            <input name="E-mail" type="email" placeholder="E-mail" required value="<?php echo $_COOKIE[$cookiename] ?? '';?>">
+            <input name = "Password" type="password" placeholder="Password" required value="<?php echo $_COOKIE[$cookiename2] ?? '';?>">
             <div class="checkbox">
             <input type ="checkbox" id="check" name="checkbox">
             <label for="check">Se souvenir de moi</label>
@@ -29,10 +31,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         $email = $_POST['E-mail'];
         $mdp = $_POST['Password'];
         if (!empty($_POST['checkbox'])){
-            $_SESSION['cacheE-mail'] = $email;
-            $_SESSION['cachePassword'] = $mdp;
+            $cookievalue = $email;
+            $cookievalue2 = $mdp;
+            setcookie($cookiename,$cookievalue,time() + (60*60*3),"/");
+            setcookie($cookiename2,$cookievalue2,time() + (60*60*2),"/");
         }else{
-            unset($_SESSION['cacheE-mail'], $_SESSION['cachePassword']);
+            setcookie($cookiename,"",time() - 9600,"/");
+            setcookie($cookiename2,"",time() - 9600, "/");
         }
         $trouve = verifUser($_POST['E-mail']);
         if ($trouve){

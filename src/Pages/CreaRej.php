769f25b0688e7677b5a53fb7ec4f80cc04234692
Milanor38 <65493965+Milanor38@users.noreply.github.com>
2024-11-24 14:54:48 +0100

    ajout de redirection dans CreaRej

--- src/Pages/CreaRej.php
+++ src/Pages/CreaRej.php
@@ -4,7 +4,7 @@
     require_once("../CRUD/CRUDPartie.php");
     require_once("../CRUD/CRUDJouerPartie.php");
     require_once("../CRUD/CRUDAppartientA.php");
-    
+    require_once("../Utils/redirection.php");
     $_SESSION['user_id'] = 1;
     $joueurTemp = readJoueur($_SESSION['user_id']);
 
